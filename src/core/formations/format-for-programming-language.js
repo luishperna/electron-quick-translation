a27d74d6e866184e719e471c7a24c184a979a738
Luís Perna <luishperna@gmail.com>
2023-10-22 16:46:56 -0300

    Ajuste as formatações JavaScript e Python

--- src/core/formations/format-for-programming-language.js
+++ src/core/formations/format-for-programming-language.js
@@ -1,4 +1,6 @@
 import { formatToCSharp } from "./programming-languages-templates/csharp.js";
+import { formatToJavaScript } from "./programming-languages-templates/javascript.js";
+import { formatToPython } from "./programming-languages-templates/python.js";
 
 export function formatForProgrammingLanguage(programmingLanguage, typeOfFormatting, text) {
     if (!typeOfFormatting) {
@@ -10,6 +12,12 @@ export function formatForProgrammingLanguage(programmingLanguage, typeOfFormatti
     switch (programmingLanguage) {
         case "CSharp":
             formattedText = formatToCSharp(typeOfFormatting, text);
+            break;
+        case "JavaScript":
+            formattedText = formatToJavaScript(typeOfFormatting, text);
+            break;
+        case "Python":
+            formattedText = formatToPython(typeOfFormatting, text);
             break;
         default:
             break;
