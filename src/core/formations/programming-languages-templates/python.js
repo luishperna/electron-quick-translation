9d1f6f9baa4881f2052b4530f8279fc6a25d7791
Luís Perna <luishperna@gmail.com>
2023-10-22 16:45:32 -0300

    Implementação da formatação para Python

---
+++ src/core/formations/programming-languages-templates/python.js
@@ -0,0 +1,24 @@
+import { toPascalCase, toSnakeCase } from "./helpers/naming-conventions.js";
+
+export function formatToPython(typeOfFormatting, text) {
+    let formattedText;
+    let type = typeOfFormatting[1].toLowerCase();
+    let subtype = typeOfFormatting[2].toLowerCase();
+
+    switch (type) {
+        case "c":
+            formattedText = `class ${toPascalCase(text)}:`;
+            break;
+        case "f":
+            formattedText = `def ${toSnakeCase(text)}():${subtype === " " ? "" : "\n\treturn"}`;
+            break;
+        case "v":
+            formattedText = `${toSnakeCase(text)} = `;
+            break;
+        default:
+            formattedText = text;
+            break;
+    }
+
+    return formattedText;
+}
